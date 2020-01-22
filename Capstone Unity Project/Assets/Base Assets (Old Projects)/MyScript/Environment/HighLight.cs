25cd661561e962ef5766c6cda6b3587a1c392de9
thenellg <griffinthenell@gmail.com>
2020-01-22 15:57:48 -0800

    Added teleportation - NEEDS TESTING

---
+++ Capstone Unity Project/Assets/Base Assets (Old Projects)/MyScript/Environment/HighLight.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighLight : MonoBehaviour
+{
+    public bool ifHighLight;
+    Color origin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        origin = gameObject.GetComponent<Renderer>().material.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ifHighLight = false;
+    }
+
+    private void LateUpdate()
+    {
+        if(ifHighLight)
+        {
+            gameObject.GetComponent<Renderer>().material.color = new Color(0, 255, 255);
+        }
+    }
+}
