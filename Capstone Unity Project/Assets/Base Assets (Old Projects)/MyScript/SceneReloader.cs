966ab891429ca4481468cb448195be5b3bcab4e6
Michael Wang <wangshiyao96@gmail.com>
2020-01-21 23:57:18 -0800

    Added workble hand and grab into the scene, updated gitignore

---
+++ Capstone Unity Project/Assets/Base Assets (Old Projects)/MyScript/SceneReloader.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SceneReloader : MonoBehaviour
+{
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); // loads current scene
+    }
+
+}
