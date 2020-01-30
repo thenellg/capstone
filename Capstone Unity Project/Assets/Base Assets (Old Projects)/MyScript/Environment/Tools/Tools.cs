b39502985c4127b8bb560410fce682ab5063eebc
Michael Wang <wangshiyao96@gmail.com>
2020-01-29 18:46:12 -0800

    Implemented prototype tools logic

---
+++ Capstone Unity Project/Assets/Base Assets (Old Projects)/MyScript/Environment/Tools/Tools.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tools : MonoBehaviour
+{
+    protected bool ifActive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ifActive = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    virtual public void Use()
+    {
+
+    }
+
+}
