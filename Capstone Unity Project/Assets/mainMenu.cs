b4103447730224bef992f866fce89698e5382ff6
thenellg <thenellg@oregonstate.edu>
2020-02-12 15:55:24 -0800

    whoops. I'm in master.

---
+++ Capstone Unity Project/Assets/mainMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class mainMenu : MonoBehaviour
+{
+    public string sceneName;
+    public string animationsName;
+
+    public Image black;
+    public Animator anim;
+
+    public void loadingScene()
+    {
+        //Put our main gameplay scene will go after this within our build.
+        StartCoroutine(fadingTransition(sceneName));
+
+    }
+
+    public void loadingAnimations()
+    {
+        //Put our main gameplay scene will go after this within our build.
+        StartCoroutine(fadingTransition(animationsName));
+
+    }
+
+    public void QuitGame ()
+    {
+        Debug.Log("Quit.");
+        Application.Quit();
+    }
+
+    IEnumerator fadingTransition(string newScene)
+    {
+        anim.SetBool("Fade", true);
+        yield return new WaitUntil(() => black.color.a == 1);
+        SceneManager.LoadScene(newScene);
+    }
+
+}
