91d55456fd0f041bfc9a4e875f5a932c32ee39fb
thenellg <thenellg@oregonstate.edu>
2020-05-23 21:41:31 -0700

    Added tutorial scene

---
+++ Capstone Unity Project/Assets/Base Assets (Old Projects)/MyScript/tutorial.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tutorial : MonoBehaviour
+{
+    public GameObject removableWall;
+    public GameObject removableWallMenu;
+
+    void Start()
+    {
+        removableWallMenu.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (OVRInput.Get(OVRInput.Button.One))
+        {
+            removableWallMenu.SetActive(true);
+            //removeWall();
+        }
+    }
+    public void removeWall()
+    {
+        Destroy(removableWall);
+        Destroy(removableWallMenu);
+    }
+
+}
